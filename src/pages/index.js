d1ba5d7e8f6b19845a98da6b73742985d8ed7ed1
twinkangaroos <twinkangaroos@gmail.com>
2023-11-19 16:05:22 +0900

    DataStoreクリア処理検証

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,120 @@
+import { Inter } from 'next/font/google'
+import { useState, useEffect} from 'react';
+import Link from 'next/link';
+import { DataStore } from '@aws-amplify/datastore';
+import { Post } from '../models';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-import { Authenticator } from '@aws-amplify/ui-react';
-import HamburgerMenu from "./HamburgerMenu"
-import Header from "./Header"
+import { Amplify, Auth } from 'aws-amplify'
+import aws_exports from '../aws-exports'
+Amplify.configure(aws_exports)
+
+const inter = Inter({ subsets: ['latin'] })
 
 function Home() {
-  return (
-    <Authenticator.Provider>
-      <Header />
-      <HamburgerMenu />
-      <h1>トップページ</h1>
-      <h3>フロントページが表示されます。</h3>
-    </Authenticator.Provider>
-  )
+    const [posts, setPosts] = useState([])
+    const [username, setUsername] = useState('')
+    const [clear_count, setClearCount] = useState(0)
+    
+    //useEffect(() => {
+        // ローカルデータのクリア
+        //clearData()
+        //setClearCount(1)
+    //}, [])
+
+    useEffect(() => {
+        //console.log("useEffect.", clear_count)
+        //if (clear_count > 0) {
+            console.log("useEffect start.")
+            fetchPosts()
+            getUsername()
+            console.log("useEffect end.")
+            //console.log("observe start.")
+            //DataStore.observe(Post).subscribe(()  => {
+            //    fetchPosts();
+            //})
+            //console.log("observe end.")
+        //}
+    }, [])
+
+    
+    async function clearData() {
+        console.log("clearData", clear_count)
+        if (clear_count === 0) {
+            console.log("clear start")
+            await DataStore.clear()
+            //await DataStore.start()
+            setClearCount(clear_count+1)
+            console.log("clear end")
+        }
+    }
+    
+
+    async function fetchPosts() {
+        try {
+            console.log("query start")
+            await DataStore.start()
+            let postData = await DataStore.query(Post)
+            setPosts(postData)
+            //setIsInit(true)
+            console.log("query end1", postData)
+            if (!postData) {
+                postData = await DataStore.query(Post);
+                setPosts(postData)
+            }
+            console.log("query end2", postData)
+        } catch(err) {
+            console.log("query中にエラー発生。", err)
+        }
+    }
+
+    async function getUsername() {
+    const user = await Auth.currentAuthenticatedUser()
+        setUsername(user.username)
+    }
+    
+    const handleSignOut = (e) => {
+        e.preventDefault()
+        Auth.signOut()
+    }
+    
+    const handleClear = async (e) => {
+        e.preventDefault()
+        clearData()
+    }
+    const handleFetch = async (e) => {
+        e.preventDefault()
+        fetchPosts()
+    }
+
+    return (
+        <div>
+        <h1>ログイン中のユーザー: {username}</h1>
+        <h2>投稿リスト</h2>
+        {
+            posts ?
+            posts.map(post => (
+            <Link key={post.id} href={`/posts/${post.id}`}>
+                <h2>{post.title}</h2>
+            </Link>
+            ))
+            :
+            ''
+        }
+        <div>
+            <a href="." onClick={handleClear}>clearData</a>
+        </div>
+        <div>
+            <a href="." onClick={handleFetch}>fetchPosts</a>
+        </div>
+        
+        <div>
+            <a href="." onClick={handleSignOut}>
+            Sign Out
+            </a>
+        </div>
+        </div>
+    )
 }
 
-export default Home
+export default withAuthenticator(Home)
